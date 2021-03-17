24e8564b06b57fd5576a47b1aa23e72384beab3d
Eduardo <em@jphlions.com>
2021-03-17 20:40:35 -0300

    assets

---
+++ assets/lan/es.js
@@ -0,0 +1,10 @@
+  $.fn.datepicker.dates['es'] = {
+            days: ["Sabado", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+            daysMin: ["Do", "Lu", "Ma", "Mi", "Jue", "Vie", "Sa"],
+            months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+            monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+            today: "Hoy",
+            clear: "Limpiar",
+            weekStart: 1
+        };
